81f708407c0f8338728570ba9390ecdc6d5a4dfb
flaviozno <flavio.vezono@ufu.br>
2024-07-26 10:05:47 -0300

    add body response

--- src/interface.ts
+++ src/interface.ts
@@ -3,4 +3,5 @@ export interface CacheEntry {
   method: string;
   statusCode: number;
   duration: number;
+  body: string;
 }
--- src/requestTimer.ts
+++ src/requestTimer.ts
@@ -8,20 +8,47 @@ const routesToIgnore = ["/socket.io", "/favicon.ico", "/traffic", "/cache"];
 export const requestTimer = (io: SocketIOServer) => {
   return (req: Request, res: Response, next: NextFunction) => {
     const start = process.hrtime();
-    res.on("finish", () => {
-      if (routesToIgnore.includes(req.url)) return;
+
+    let oldWrite = res.write;
+    let oldEnd = res.end;
+
+    const chunks: Buffer[] = [];
+
+    res.write = function (chunk: any, ...args: any[]) {
+      chunks.push(Buffer.isBuffer(chunk) ? chunk : Buffer.from(chunk));
+      return oldWrite.apply(res, [chunk, ...args] as any);
+    };
+
+    res.end = function (chunk: any, ...args: any[]) {
+      if (chunk) {
+        chunks.push(Buffer.isBuffer(chunk) ? chunk : Buffer.from(chunk));
+      }
+
+      const body = Buffer.concat(chunks).toString("utf8");
       const [seconds, nanoseconds] = process.hrtime(start);
       const duration = Number((seconds * 1e3 + nanoseconds * 1e-6).toFixed(3));
       const method = req.method || "UNKNOWN";
       const url = req.url || "UNKNOWN";
       const statusCode = res.statusCode || 404;
-      const logEntry: CacheEntry = { method, url, statusCode, duration };
-      if (!cache[url]) {
-        cache[url] = [];
+
+      if (!routesToIgnore.includes(url)) {
+        const logEntry: CacheEntry = {
+          method,
+          url,
+          statusCode,
+          duration,
+          body,
+        };
+        if (!cache[url]) {
+          cache[url] = [];
+        }
+        cache[url].push(logEntry);
+        io.emit("traffic", logEntry);
       }
-      cache[url].push(logEntry);
-      io.emit("traffic", logEntry);
-    });
+
+      return oldEnd.apply(res, [chunk, ...args] as any);
+    };
+
     next();
   };
 };
